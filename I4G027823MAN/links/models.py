126fe141fc011ae1cbbe76b33e132285e3ac7053
Rufai AbdulWasii <rufaiwasiu5@gmail.com>
2022-07-19 13:54:25 +0100

    This is a new repository

---
+++ I4G027823MAN/links/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from .managers import ActiveLinkManager
+
+# Create your models here.
+
+class Links(models.Model):
+    target_URL = models.URLField(max_length=200)
+    description = models.CharField(max_length=200)
+    identifier = models.SlugField(max_length=200, blank=True, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    objects = models.Manager()
+    public = ActiveLinkManager()
